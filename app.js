f176890908e66e60a8e91dab36135dec79d881a7
iarleisouza <65626531+iarleisouza@users.noreply.github.com>
2024-03-16 09:47:28 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
+// importar o módulo fs - modul oque o nodejs permite que utilize arquivo no sistema operacional
 const fs = require("fs")
 
 const readFromFile = fileName => {
     if (!fs.existsSync(fileName)){
         return null
     }
-
+// métododo readFileSync
     return fs.readFileSync(fileName, 'utf-8')
 }
-
+// ler o arquivo JSON  - funções de leitura 
 const readJSON = fileName => {
+    //função ler do arquivo
     const readFile = readFromFile(fileName)
 
     if (!readFile) {
         return readFile
     }
+    // JSON.parse converter de JSON para javascript
     return JSON.parse(readFile)
 }
 
+// funcão responsável para escrita de arquivos
 const write = ({ data, fileName}) => {
-fs.writeFileSync(fileName, JSON.stringify(data))
+fs.writeFileSync(fileName, JSON.stringify(data)) //  JSON.stringify converte o dado javascript para JSON
 }
 
 const value = [
@@ -31,7 +35,7 @@ const value = [
         numero: 100
     }
 ]
-
-// write({ data: value, fileName: "dados.json"})
+// chamar a função write e passar o objeto
+//write({ data: value, fileName: "dados.json"})
 
 console.log(readJSON('dados.json'))
